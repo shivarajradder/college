94b9ee4024973bf738db8366827bc29cd12f17c6
Shivaraj Radder <shivaraj.radder@ibm.com>
2022-06-06 12:46:49 +0530

    modified:   student.class 	modified:   student.java 	modified:   teacher.class 	modified:   teacher.java

--- student.java
+++ student.java
@@ -3,5 +3,6 @@ class student{
 public static void main(String[] args){
 
 System.out.println("I am a Student");
+System.out.println("I am studying at 8th standard");
 }
 }
--- teacher.java
+++ teacher.java
@@ -3,5 +3,6 @@ class teacher{
 public static void main(String[] args){
 
 System.out.println("I am a Teacher");
+System.out.println("I teach for 8th standard students");
 }
 }
