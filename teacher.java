178de645cbe715517814edf836dd972ba148f6d0
Shivaraj Radder <shivaraj.radder@ibm.com>
2022-06-06 12:53:37 +0530

    modified:   teacher.java

--- teacher.java
+++ teacher.java
@@ -4,5 +4,7 @@ public static void main(String[] args){
 
 System.out.println("I am a Teacher");
 System.out.println("I teach for 8th standard students");
+System.out.println("I teach Maths for 8th standard students");
+
 }
 }
